76ba896d2c81c3ceb13c97a14581ed47a1da9f81
Autokaka <785321595@qq.com>
2019-08-30 21:30:03 +0800

    Organized all the resources and give just one punch!

--- README.md
+++ README.md
@@ -1,6 +1,17 @@
-# biliconnectiontest
+# bili-live-connector
+
+This is a vue project built by vue-cli3.0, served as an example to connect to any Bilibili live room in browser if you want. And the basic functions such as danmaku, gift displaying, welcome message, global broadcast and popularity report has been realized. You can see the detailed message either by inspecting the console or just seeing the printed message in the static page (index.html).
+
+The static page can function well in very early browsers after thousand of testing, I am pretty confident that there's no need to worry about compatibility, =v=. Anyway, if there were any broblem while using this stuff, just post an issue and I'll handle it.
+
+The connection is based on websocket, and most part of its techniques are coming from  [Bili-Live-API](https://github.com/lovelyyoshino/Bilibili-Live-API). To be honest, I just draw a dipper with a gourd as a model, orz.
+
+But this is one of the part that I use to realize my customized live plugins for Bilibili ups. I cannot give the whole source of the project because of the appointment, this part, however, is the core part of all my tricks, I'd like to share it with you all~ I wonder if I am the first one to make an OBS plugin in that way, haha~
+
+The inititialization is quite simple, just follow the instructions below: 
 
 ## Project setup
+
 ```
 npm install
 ```
--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,151 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <ul>
+      <div v-for="log in logs" :key="log.id">{{log.message}}</div>
+    </ul>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
+  data: function() {
+    return {
+      logs: []
+    };
+  },
+  methods: {
+    addNewLog(sth) {
+      this.logs.push({
+        message: sth
+      });
+    }
+  },
+  mounted: function() {
+    const _this = this;
+    var TextEncoder = require("text-encoder-lite").TextEncoderLite;
+    var TextDecoder = require("text-encoder-lite").TextDecoderLite;
+    const textEncoder = new TextEncoder("utf-8");
+    const textDecoder = new TextDecoder("utf-8");
+
+    const readInt = function(buffer, start, len) {
+      let result = 0;
+      for (let i = len - 1; i >= 0; i--) {
+        result += Math.pow(256, len - i - 1) * buffer[start + i];
+      }
+      return result;
+    };
+    const writeInt = function(buffer, start, len, value) {
+      let i = 0;
+      while (i < len) {
+        buffer[start + i] = value / Math.pow(256, len - i - 1);
+        i++;
+      }
+    };
+
+    const encode = function(str, op) {
+      let data = textEncoder.encode(str);
+      let packetLen = 16 + data.byteLength;
+      let header = [0, 0, 0, 0, 0, 16, 0, 1, 0, 0, 0, op, 0, 0, 0, 1];
+      writeInt(header, 0, 4, packetLen);
+      return new Uint8Array(header.concat(...data)).buffer;
+    };
+    const decode = function(blob) {
+      return new Promise(function(resolve, reject) {
+        let reader = new FileReader();
+        reader.onload = function(e) {
+          let buffer = new Uint8Array(e.target.result);
+          let result = {};
+          result.packetLen = readInt(buffer, 0, 4);
+          result.headerLen = readInt(buffer, 4, 2);
+          result.ver = readInt(buffer, 6, 2);
+          result.op = readInt(buffer, 8, 4);
+          result.seq = readInt(buffer, 12, 4);
+          if (result.op === 5) {
+            result.body = [];
+            let offset = 0;
+            while (offset < buffer.length) {
+              let packetLen = readInt(buffer, offset + 0, 4);
+              let headerLen = 16; // readInt(buffer,offset + 4,4)
+              let data = buffer.slice(offset + headerLen, offset + packetLen);
+              let body = textDecoder.decode(data);
+              if (body) {
+                result.body.push(JSON.parse(body));
+              }
+              offset += packetLen;
+            }
+          } else if (result.op === 3) {
+            result.body = {
+              count: readInt(buffer, 16, 4)
+            };
+          }
+          resolve(result);
+        };
+        reader.readAsArrayBuffer(blob);
+      });
+    };
+
+    //发送handshake，代号7
+    const ws = new WebSocket("wss://broadcastlv.chat.bilibili.com:2245/sub");
+    ws.onopen = function() {
+      ws.send(
+        encode(
+          JSON.stringify({
+            roomid: 360972
+          }),
+          7
+        )
+      );
+    };
+
+    //发送心跳包，代号2
+    setInterval(function() {
+      ws.send(encode("", 2));
+    }, 30000);
+
+    //接收心跳包实时数据
+    ws.onmessage = async function(msgEvent) {
+      const packet = await decode(msgEvent.data);
+      switch (packet.op) {
+        case 8:
+          console.log("加入房间");
+          break;
+        case 3:
+          const count = packet.body.count;
+          console.log(`人气：${count}`);
+          break;
+        case 5:
+          packet.body.forEach(body => {
+            switch (body.cmd) {
+              case "DANMU_MSG":
+                console.log(`${body.info[2][1]}: ${body.info[1]}`);
+                _this.addNewLog(`${body.info[2][1]}: ${body.info[1]}`);
+                break;
+              case "SEND_GIFT":
+                console.log(
+                  `${body.data.uname} ${body.data.action} ${body.data.num} 个 ${body.data.giftName}`
+                );
+                _this.addNewLog(
+                  `${body.data.uname} ${body.data.action} ${body.data.num} 个 ${body.data.giftName}`
+                );
+                break;
+              case "WELCOME":
+                console.log(`欢迎 ${body.data.uname}`);
+                _this.addNewLog(`欢迎 ${body.data.uname}`);
+                break;
+              // 此处省略很多其他通知类型
+              default:
+                console.log(`其他通知: `, body);
+                break;
+            }
+          });
+          break;
+        default:
+          console.log(packet);
+      }
+    };
   }
-}
+};
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
